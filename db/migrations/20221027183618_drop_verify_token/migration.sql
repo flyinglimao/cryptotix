8367fe8a25f4420db9ababf8bed55be460bc1b84
flyinglimao <hi@limaois.me>
2022-10-28 02:36:38 +0800

    chore: drop unused model

---
+++ db/migrations/20221027183618_drop_verify_token/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the `VerifyToken` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "VerifyToken";
+PRAGMA foreign_keys=on;
